40908a349a4f2313f3e9fe656543cff7e633b699
unknown <saad.binabid@palmira-itc.com>
2024-03-06 21:55:40 +0500

    thumbnails and videos related work done

--- src/app/yt-info/yt-info.component.ts
+++ src/app/yt-info/yt-info.component.ts
@@ -10,6 +10,7 @@ export class YtInfoComponent implements OnInit{
 
   @Input() ytType: string | undefined;
   public allThumbnails:any = [];
+  allVideos: any = [];
 
   constructor(
     private ytApiService: YtApiService
@@ -22,20 +23,27 @@ export class YtInfoComponent implements OnInit{
     const urle = 'https://www.youtube.com/watch?v=uoFDy_187zQ';
     this.ytApiService.getData(urle,typeOfContent).subscribe((res)=>{
       if(res) {
-        this.allThumbnails = this.ytType == 'thumbnails' && res;
+        this.allThumbnails = this.ytType == 'thumbnails' ? res : [];
+        this.allVideos = this.ytType == 'videos' ? res : [];
         console.log('ytApiServiceytApiService',res)
         console.log('ytApiServiceytApiService',this.allThumbnails)
       }
     })
   }
 
-  downloadImage(imageUrl: string): void {
-    const link = document.createElement('a');
-    link.href = imageUrl;
-    link.download = 'image.jpg';
-    document.body.appendChild(link);
-    link.click();
-    window.open(imageUrl, '_blank');
-    document.body.removeChild(link);
+  downloadData(url: string, type: string): void {
+    if(type == 'thumbnails'){
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = 'image.jpg';
+      document.body.appendChild(link);
+      link.click();
+      window.open(url, '_blank');
+      document.body.removeChild(link);
+    } else if(type == 'videos'){
+      console.log('videosvideosvideosvideos',url)
+    } else {
+      
+    }
   }
 }
